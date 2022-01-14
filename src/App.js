1f9992166dec02ad5334e1673235a3307e258230
lmohrs <lmohrs@purdu.edu>
2022-01-14 10:22:48 -0500

    Added cocktail pictures. Created empty files for the App and Navbar.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import sexOnTheBeach from './Pictures/Sex_on_the_Beach.jpg';
+import longIsland from './Pictures/Long_Island.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+        <img src = {sexOnTheBeach} className="App-sexOnTheBeach" alt = "Sex on the Beach"/>
+           <img src = {longIsland} className="App-longIsland" alt = "Long Island Ice Tea"/>
+        </header>
     </div>
-  );
+  )
 }
 
 export default App;
+
+{/*<header className="App-header">*/}
+{/*  <img src={logo} className="App-logo" alt="logo" />*/}
+{/*  <p>*/}
+{/*    Edit <code>src/App.js</code> and save to reload.*/}
+{/*  </p>*/}
+{/*  <a*/}
+{/*    className="App-link"*/}
+{/*    href="https://reactjs.org"*/}
+{/*    target="_blank"*/}
+{/*    rel="noopener noreferrer"*/}
+{/*  >*/}
+{/*    Learn React*/}
+{/*  </a>*/}
+{/*</header>*/}
